560fa75cd221a2d15df21d53b362a92b4a894400
Kazuya Nakazato <magmanakazato@gmail.com>
2022-01-02 23:15:22 +0900

    電卓アプリの画面作成 (#5)
    * fix #1 SwiftLint導入
    
    * .swiftLint.yml微修正
    
    * .swiftLint.yml微修正。SwiftUIだとZStackのような長いクロージャを持ってしまうところは無効化
    
    * 数字を表示するところとボタンを配置
    
    * 見た目微修正とコード整理

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let calculateItems: [[String]] = [
+        ["C", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", "="]
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("0")
+                    .modifier(CalculateResultModifier())
+                VStack {
+                    ForEach(calculateItems, id: \.self) { items in
+                        HStack {
+                            ForEach(items, id: \.self) { item in
+                                Button(action: {}) {
+                                    Text(item)
+                                        .font(.system(size: 30, weight: .regular))
+                                        .frame(
+                                            minWidth: 0,
+                                            maxWidth: .infinity,
+                                            minHeight: 0,
+                                            maxHeight: .infinity
+                                        )
+                                }
+                                .modifier(ButtonModifier(item: item))
+                            }
+                        }
+                    }
+                }.padding(.bottom, 40)
+            }
+        }
     }
 }
 
