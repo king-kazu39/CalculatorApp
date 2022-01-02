662c47afda72b2f91f9595b327f03145afa0d22c
king-kazu39 <magmanakazato@gmail.com>
2022-01-02 13:57:54 +0900

    first commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by kazuya on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
