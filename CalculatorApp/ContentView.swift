d979ed75a20124fb3ac332ae5156bd4913f09e7c
king-kazu39 <magmanakazato@gmail.com>
2022-01-02 19:04:47 +0900

    数字を表示するところとボタンを配置

--- CalculatorApp/ContentView.swift
+++ CalculatorApp/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    let  buttonWidth: CGFloat = (UIScreen.main.bounds.width - 50) / 4
+    let calculateItems: [[String]] = [
+        ["C", "", "", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", "", "="]
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("0")
+                    .foregroundColor(.white)
+                    .font(.system(size: 100, weight: .light))
+                    .padding()
+                    .lineLimit(1)
+                    .frame(
+                        width: UIScreen.main.bounds.width - 50,
+                        height: 100,
+                        alignment: .trailing
+                    )
+                    .minimumScaleFactor(0.4)
+                VStack {
+                    ForEach(calculateItems, id: \.self) { items in
+                        HStack {
+                            ForEach(items, id: \.self) { item in
+                                Button(action: {}) {
+                                    Text(item)
+                                        .font(.system(size: 30, weight: .regular))
+                                        .frame(
+                                            minWidth: 0,
+                                            maxWidth: .infinity,
+                                            minHeight: 0,
+                                            maxHeight: .infinity
+                                        )
+                                }
+                                .foregroundColor(.white)
+                                .background(Color(white: 0.2, opacity: 1))
+                                .frame(width: buttonWidth)
+                                .cornerRadius(buttonWidth)
+                            }
+                        }
+                    }
+                }.padding(.bottom, 40)
+            }
+        }
     }
 }
 
